932fc80bce64326254c508e3b699769bbe73d890
willbraun <williamhbraun1@gmail.com>
2023-09-27 07:54:47 -0400

    getting data from first round

--- main.go
+++ main.go
@@ -2,25 +2,22 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
 )
 
+type slot struct {
+	draw_id     string
+	round       int
+	position    int
+	player_name string
+	seed        string
+}
+
 func main() {
-	// get HTML from URL
-	// format HTML into struct
-
-	type slot struct {
-		draw_id     string
-		player_name string
-		seed        string
-		round       int
-		position    int
-	}
-
-	slots := []slot{}
-
 	fmt.Println("Started")
 	c := colly.NewCollector()
 
@@ -36,13 +33,30 @@ func main() {
 		fmt.Println("Response code:", r.StatusCode)
 	})
 
+	// get HTML from URL
+	// format HTML into struct
+
+	slots := []slot{}
+
 	c.OnHTML(".scores-draw-entry-box", func(e *colly.HTMLElement) {
 		table := e.DOM.ChildrenMatcher(goquery.Single(".scores-draw-entry-box-table"))
 		if table.Length() > 0 {
-			// add each player from round 1
+			rows := table.ChildrenMatcher(goquery.Single("tbody")).Children()
+			rows.Each(func(i int, row *goquery.Selection) {
+				values := row.Children().Map(func(i int, s *goquery.Selection) string {
+					return trim(s.Text())
+				})
+				position, seed, name := values[0], values[1], values[2]
+				positionInt, err := strconv.Atoi(position)
 
-			entry := slot{draw_id: "1", player_name: "test", seed: "1", round: 1, position: 1}
-			slots = append(slots, entry)
+				if err != nil {
+					fmt.Println(err)
+				}
+
+				entry := slot{draw_id: "1", round: 1, position: positionInt, player_name: name, seed: seed }
+				slots = append(slots, entry)
+			})
+
 		} else {
 			// add the single player in the other rounds
 		}
@@ -55,7 +69,12 @@ func main() {
 
 	c.Visit("https://www.atptour.com/en/scores/current/chengdu/7581/draws")
 
+	fmt.Println(slots)
 	// get current slots from pb, script_user
 	// filter struct to remove positions already in pb
 	// loop over remaining struct to upload to pb
+}
+
+func trim(s string) string {
+	return strings.Trim(s, " \n\r")
 }
