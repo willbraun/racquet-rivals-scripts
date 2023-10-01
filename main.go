fca7ade8cd8531013b482aa3c4aacfa619121c27
willbraun <williamhbraun1@gmail.com>
2023-10-01 15:12:52 -0400

    posting slots to database, cleaned up api calls

--- main.go
+++ main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"net/http"
-	"os"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
@@ -16,55 +11,14 @@ import (
 )
 
 type Slot struct {
-	Draw_id     string
-	Round       int
-	Position    int
-	Player_name string
-	Seed        string
+	Draw_id  string
+	Round    int
+	Position int
+	Name     string
+	Seed     string
 }
 
 type slotSlice []Slot
-
-type UserRecord struct {
-	ID              string `json:"id"`
-	CollectionID    string `json:"collectionId"`
-	CollectionName  string `json:"collectionName"`
-	Created         string `json:"created"`
-	Updated         string `json:"updated"`
-	Username        string `json:"username"`
-	Email           string `json:"email"`
-	Verified        bool   `json:"verified"`
-	EmailVisibility bool   `json:"emailVisibility"`
-	SomeCustomField string `json:"someCustomField"`
-}
-
-type UserAuthRes struct {
-	Token  string     `json:"token"`
-	Record UserRecord `json:"record"`
-}
-
-type DrawRecord struct {
-	ID               string `json:"id"`
-	CollectionID     string `json:"collectionId"`
-	CollectionName   string `json:"collectionName"`
-	Name             string `json:"name"`
-	Event            string `json:"event"`
-	Year             int    `json:"year"`
-	Url              string `json:"url"`
-	Start_Date       string `json:"start_date"`
-	End_Date         string `json:"end_date"`
-	Prediction_Close string `json:"prediction_close"`
-	Updated          string `json:"updated"`
-	Created          string `json:"created"`
-}
-
-type DrawRes struct {
-	Page       int          `json:"page"`
-	PerPage    int          `json:"perPage"`
-	TotalItems int          `json:"totalItems"`
-	TotalPages int          `json:"totalPages"`
-	Items      []DrawRecord `json:"items"`
-}
 
 func main() {
 	err := godotenv.Load()
@@ -72,9 +26,10 @@ func main() {
 		fmt.Println("Error loading .env file")
 	}
 
-	// fmt.Println(os.Getenv("SCRIPT_USER_USERNAME"))
+	token := Login()
+	draws := getDraws(token)
+	testDraw := draws[0]
 
-	fmt.Println("Started")
 	c := colly.NewCollector()
 
 	c.OnRequest(func(r *colly.Request) {
@@ -110,7 +65,7 @@ func main() {
 					fmt.Println(err)
 				}
 
-				slots.add(Slot{Draw_id: "1", Round: 1, Position: position, Player_name: name, Seed: seed})
+				slots.add(Slot{Draw_id: testDraw.ID, Round: 1, Position: position, Name: name, Seed: seed})
 
 				seeds[name] = seed
 				currentRound = 2
@@ -123,7 +78,7 @@ func main() {
 			name := trim(e.DOM.ChildrenMatcher(goquery.Single(".scores-draw-entry-box-players-item")).Text())
 			seed := seeds[name]
 
-			slots.add(Slot{Draw_id: "1", Round: round, Position: position, Player_name: name, Seed: seed})
+			slots.add(Slot{Draw_id: testDraw.ID, Round: round, Position: position, Name: name, Seed: seed})
 
 			currentRound++
 		}
@@ -133,63 +88,13 @@ func main() {
 		fmt.Println("Finished scraping", r.Request.URL)
 	})
 
-	// c.Visit("https://www.atptour.com/en/scores/current/beijing/747/draws")
-
-	// fmt.Println(slots)
-
-	// get current slots from pb, script_user
-	authUrl := "https://tennisbracket.willbraun.dev/api/collections/user/auth-with-password"
-	username := os.Getenv("SCRIPT_USER_USERNAME")
-	password := os.Getenv("SCRIPT_USER_PASSWORD")
-
-	authRequestData := struct {
-		Identity string `json:"identity"`
-		Password string `json:"password"`
-	}{
-		Identity: username,
-		Password: password,
-	}
-
-	authJSON, err := json.Marshal(authRequestData)
-	perror(err)
-
-	body := []byte(authJSON)
-
-	userRequest, err := http.NewRequest("POST", authUrl, bytes.NewBuffer(body))
-	perror(err)
-
-	userRequest.Header.Add("Content-Type", "application/json")
-
-	authClient := &http.Client{Timeout: 10 * time.Second}
-	ures, err := authClient.Do(userRequest)
-	perror(err)
-
-	defer ures.Body.Close()
-
-	userAuthRes := &UserAuthRes{}
-	fmt.Println("auth request status:", ures.Status)
-	uderr := json.NewDecoder(ures.Body).Decode(userAuthRes)
-	perror(uderr)
-
-	token := userAuthRes.Token
-
-	getDrawsUrl := "https://tennisbracket.willbraun.dev/api/collections/draw/records"
-	drawRequest, err := http.NewRequest("GET", getDrawsUrl, nil)
-	drawRequest.Header.Add("Authorization", token)
-	drawClient := &http.Client{Timeout: 10 * time.Second}
-	dres, err := drawClient.Do(drawRequest)
-	perror(err)
-
-	defer dres.Body.Close()
-
-	drawRes := &DrawRes{}
-	dderr := json.NewDecoder(dres.Body).Decode(drawRes)
-	perror(dderr)
-
-	fmt.Println(drawRes.Items)
+	fmt.Println("Start scraping")
+	c.Visit(testDraw.Url)
 
 	// filter struct to remove positions already in pb
 	// loop over remaining struct to upload to pb
+
+	postSlots(slots, token)
 }
 
 func trim(s string) string {
@@ -198,10 +103,4 @@ func trim(s string) string {
 
 func (ss *slotSlice) add(s Slot) {
 	*ss = append(*ss, s)
-}
-
-func perror(err error) {
-	if err != nil {
-		fmt.Println(err)
-	}
 }
---
+++ pocketbase.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+type UserRecord struct {
+	ID              string `json:"id"`
+	CollectionID    string `json:"collectionId"`
+	CollectionName  string `json:"collectionName"`
+	Created         string `json:"created"`
+	Updated         string `json:"updated"`
+	Username        string `json:"username"`
+	Email           string `json:"email"`
+	Verified        bool   `json:"verified"`
+	EmailVisibility bool   `json:"emailVisibility"`
+	SomeCustomField string `json:"someCustomField"`
+}
+
+type UserAuthRes struct {
+	Token  string     `json:"token"`
+	Record UserRecord `json:"record"`
+}
+
+type DrawRecord struct {
+	ID               string `json:"id"`
+	Name             string `json:"name"`
+	Event            string `json:"event"`
+	Year             int    `json:"year"`
+	Url              string `json:"url"`
+	Start_Date       string `json:"start_date"`
+	End_Date         string `json:"end_date"`
+	Prediction_Close string `json:"prediction_close"`
+}
+
+type DrawRes struct {
+	Page       int          `json:"page"`
+	PerPage    int          `json:"perPage"`
+	TotalItems int          `json:"totalItems"`
+	TotalPages int          `json:"totalPages"`
+	Items      []DrawRecord `json:"items"`
+}
+
+type CreateSlotReq struct {
+	Draw_id  string `json:"draw_id"`
+	Round    int    `json:"round"`
+	Position int    `json:"position"`
+	Name     string `json:"name"`
+	Seed     string `json:"seed"`
+}
+
+type CreateSlotRes struct {
+	ID             string `json:"id"`
+	CollectionID   string `json:"collectionId"`
+	CollectionName string `json:"collectionName"`
+	Created        string `json:"created"`
+	Updated        string `json:"updated"`
+	DrawID         string `json:"draw_id"`
+	Name           string `json:"name"`
+	Seed           int    `json:"seed"`
+	Round          int    `json:"round"`
+	Position       int    `json:"position"`
+}
+
+func makeHTTPRequest(method, url, token string, requestData interface{}) (*http.Response, error) {
+	body, err := json.Marshal(requestData)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(body))
+	if err != nil {
+		return nil, err
+	}
+
+	if token != "" {
+		req.Header.Add("Authorization", token)
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+
+	client := &http.Client{Timeout: 10 * time.Second}
+	return client.Do(req)
+}
+
+func Login() string {
+	url := "https://tennisbracket.willbraun.dev/api/collections/user/auth-with-password"
+
+	requestData := struct {
+		Identity string `json:"identity"`
+		Password string `json:"password"`
+	}{
+		Identity: os.Getenv("SCRIPT_USER_USERNAME"),
+		Password: os.Getenv("SCRIPT_USER_PASSWORD"),
+	}
+
+	res, err := makeHTTPRequest("POST", url, "", requestData)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	defer res.Body.Close()
+
+	userAuthRes := &UserAuthRes{}
+	fmt.Println("auth request status:", res.Status)
+	derr := json.NewDecoder(res.Body).Decode(userAuthRes)
+	if derr != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	return userAuthRes.Token
+}
+
+func getDraws(token string) []DrawRecord {
+	url := "https://tennisbracket.willbraun.dev/api/collections/draw/records?fields=id,name,event,year,url,start_date,end_date,prediction_close"
+	res, err := makeHTTPRequest("GET", url, token, nil)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	defer res.Body.Close()
+
+	drawRes := &DrawRes{}
+	derr := json.NewDecoder(res.Body).Decode(drawRes)
+	if derr != nil {
+		fmt.Println(derr)
+		return nil
+	}
+
+	return drawRes.Items
+}
+
+func postSlots(slots slotSlice, token string) {
+	url := "https://tennisbracket.willbraun.dev/api/collections/draw_slot/records"
+
+	for _, v := range slots {
+		requestData := CreateSlotReq{
+			Draw_id:  v.Draw_id,
+			Round:    v.Round,
+			Position: v.Position,
+			Name:     v.Name,
+			Seed:     v.Seed,
+		}
+		res, err := makeHTTPRequest("POST", url, token, requestData)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer res.Body.Close()
+
+		fmt.Println(res.Status, v)
+	}
+}
