7acf9a5b3f16d08ba952d198d857162eba6cdc15
willbraun <williamhbraun1@gmail.com>
2023-11-29 08:43:40 -0500

    trying to scrape WTA internal API for winner

--- scrapers.go
+++ scrapers.go
@@ -4,9 +4,99 @@ import (
 	"fmt"
 	"strconv"
 
+	// "time"
+
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
 )
+
+// type WTAMatchData struct {
+// 	BinPacketBase64   string    `json:"BinPacketBase64"`
+// 	CourtID           int       `json:"CourtID"`
+// 	DateSeq           int       `json:"DateSeq"`
+// 	DrawLevelType     string    `json:"DrawLevelType"`
+// 	DrawMatchType     string    `json:"DrawMatchType"`
+// 	EntryTypeA        string    `json:"EntryTypeA"`
+// 	EntryTypeB        string    `json:"EntryTypeB"`
+// 	EventID           string    `json:"EventID"`
+// 	EventYear         int       `json:"EventYear"`
+// 	LastUpdated       time.Time `json:"LastUpdated"`
+// 	MatchID           string    `json:"MatchID"`
+// 	MatchState        string    `json:"MatchState"`
+// 	MatchTimeStamp    time.Time `json:"MatchTimeStamp"`
+// 	MatchTimeTotal    string    `json:"MatchTimeTotal"`
+// 	Message           string    `json:"Message"`
+// 	NumSets           int       `json:"NumSets"`
+// 	PlayerCountryA    string    `json:"PlayerCountryA"`
+// 	PlayerCountryA2   string    `json:"PlayerCountryA2"`
+// 	PlayerCountryB    string    `json:"PlayerCountryB"`
+// 	PlayerCountryB2   string    `json:"PlayerCountryB2"`
+// 	PlayerIDA         string    `json:"PlayerIDA"`
+// 	PlayerIDA2        string    `json:"PlayerIDA2"`
+// 	PlayerIDB         string    `json:"PlayerIDB"`
+// 	PlayerIDB2        string    `json:"PlayerIDB2"`
+// 	PlayerNameFirstA  string    `json:"PlayerNameFirstA"`
+// 	PlayerNameFirstA2 string    `json:"PlayerNameFirstA2"`
+// 	PlayerNameFirstB  string    `json:"PlayerNameFirstB"`
+// 	PlayerNameFirstB2 string    `json:"PlayerNameFirstB2"`
+// 	PlayerNameLastA   string    `json:"PlayerNameLastA"`
+// 	PlayerNameLastA2  string    `json:"PlayerNameLastA2"`
+// 	PlayerNameLastB   string    `json:"PlayerNameLastB"`
+// 	PlayerNameLastB2  string    `json:"PlayerNameLastB2"`
+// 	PointA            string    `json:"PointA"`
+// 	PointB            string    `json:"PointB"`
+// 	ResultString      string    `json:"ResultString"`
+// 	RoundID           string    `json:"RoundID"`
+// 	ScoreSet1A        string    `json:"ScoreSet1A"`
+// 	ScoreSet1B        string    `json:"ScoreSet1B"`
+// 	ScoreSet2A        string    `json:"ScoreSet2A"`
+// 	ScoreSet2B        string    `json:"ScoreSet2B"`
+// 	ScoreSet3A        string    `json:"ScoreSet3A"`
+// 	ScoreSet3B        string    `json:"ScoreSet3B"`
+// 	ScoreSet4A        string    `json:"ScoreSet4A"`
+// 	ScoreSet4B        string    `json:"ScoreSet4B"`
+// 	ScoreSet5A        string    `json:"ScoreSet5A"`
+// 	ScoreSet5B        string    `json:"ScoreSet5B"`
+// 	ScoreString       string    `json:"ScoreString"`
+// 	ScoreSys          string    `json:"ScoreSys"`
+// 	ScoreTbSet1       string    `json:"ScoreTbSet1"`
+// 	ScoreTbSet2       string    `json:"ScoreTbSet2"`
+// 	ScoreTbSet3       string    `json:"ScoreTbSet3"`
+// 	ScoreTbSet4       string    `json:"ScoreTbSet4"`
+// 	SeedA             string    `json:"SeedA"`
+// 	SeedB             string    `json:"SeedB"`
+// 	Serve             string    `json:"Serve"`
+// 	Winner            string    `json:"Winner"`
+// }
+
+// type WTATournamentData struct {
+// 	TournamentGroup   WTATournamentGroupData `json:"tournamentGroup"`
+// 	Year              int             `json:"year"`
+// 	Title             string          `json:"title"`
+// 	StartDate         string          `json:"startDate"`
+// 	EndDate           string          `json:"endDate"`
+// 	Surface           string          `json:"surface"`
+// 	InOutdoor         string          `json:"inOutdoor"`
+// 	City              string          `json:"city"`
+// 	Country           string          `json:"country"`
+// 	SinglesDrawSize   int             `json:"singlesDrawSize"`
+// 	DoublesDrawSize   int             `json:"doublesDrawSize"`
+// 	PrizeMoney        int             `json:"prizeMoney"`
+// 	PrizeMoneyCurrency string          `json:"prizeMoneyCurrency"`
+// 	LiveScoringId     string          `json:"liveScoringId"`
+// }
+
+// type WTATournamentGroupData struct {
+// 	ID       int    `json:"id"`
+// 	Name     string `json:"name"`
+// 	Level    string `json:"level"`
+// 	Metadata interface{} `json:"metadata"`
+// }
+
+// type WTADataResponse struct {
+// 	Matches    []WTAMatchData `json:"matches"`
+// 	Tournament WTATournamentData     `json:"tournament"`
+// }
 
 func scrapeATP(draw DrawRecord) (slotSlice, map[string]string) {
 	slots := slotSlice{}
@@ -80,6 +170,9 @@ func scrapeWTA(draw DrawRecord) (slotSlice, map[string]string) {
 	slots := slotSlice{}
 	seeds := make(map[string]string)
 
+	// wtaDrawId := strings.Split(draw.Url, "/")[4]
+	// fmt.Println(wtaDrawId)
+
 	c := colly.NewCollector()
 
 	c.OnRequest(func(r *colly.Request) {
@@ -115,6 +208,23 @@ func scrapeWTA(draw DrawRecord) (slotSlice, map[string]string) {
 				})
 			})
 		})
+
+		// url := fmt.Sprintf(`https://api.wtatennis.com/tennis/tournaments/%s/2023/matches/?states=%s`, wtaDrawId, "L%2C%20C")
+		// fmt.Println(url)
+		// res, err := makeHTTPRequest("GET", url, "", nil)
+		// defer res.Body.Close()
+
+		// wtaDataResponse := &WTADataResponse{}
+		// fmt.Println("Auth request status:", res.Status)
+		// derr := json.NewDecoder(res.Body).Decode(wtaDataResponse)
+		// if derr != nil {
+		// 	fmt.Println(err)
+		// 	return
+		// }
+
+		// matches := wtaDataResponse.Matches
+		// winner := strings.Split(matches[len(matches)-1].ResultString, " d ")[0]
+		// fmt.Println(winner)
 
 		champion := roundContainers.Last().Find(".is-winner").Find(".match-table__player-name")
 		name, seed := wtaExtractRow(champion)
