166cd78db6515c503554cdbe3e24aaca7bd5731c
willbraun <williamhbraun1@gmail.com>
2025-02-03 08:03:08 -0500

    updated to new draw to pass tests

--- scrapers_test.go
+++ scrapers_test.go
@@ -16,27 +16,28 @@ func TestScrapeATP(t *testing.T) {
 		log.Println("Error loading .env file,", err)
 	}
 
+	// If this fails, change the draw URL to a current draw (with current in the URL)
 	draw := DrawRecord{
 		ID:               "abc123",
-		Name:             "Brisbane International",
+		Name:             "Australian Open",
 		Event:            "Men's Singles",
-		Year:             2024,
-		Url:              "https://www.atptour.com/en/scores/archive/brisbane/339/2024/draws",
-		Start_Date:       "2023-12-31 12:00:00.000",
-		End_Date:         "2024-01-07 12:00:00.000",
-		Prediction_Close: "2024-01-03 12:00:00.000",
-		Size:             32,
+		Year:             2025,
+		Url:              "https://www.atptour.com/en/scores/current/australian-open/580/draws",
+		Start_Date:       "2025-01-12 12:00:00.000",
+		End_Date:         "2024-01-26 12:00:00.000",
+		Prediction_Close: "2024-01-19 12:00:00.000",
+		Size:             128,
 	}
 
 	t.Run("Scrape ATP", func(t *testing.T) {
 		scrapedSlots, seeds := scrapeATP(draw)
 		assert := assert.New(t)
 
-		assert.Equal(len(scrapedSlots), 63)
-		for _, slot := range scrapedSlots {
-			assert.NotEmpty(slot.Name)
-		}
-		assert.Equal(len(seeds), 32)
+		assert.Equal(len(scrapedSlots), 255)
+
+		// If the draw is not complete, there will be an extra empty seed representing empty slots
+		delete(seeds, "")
+		assert.Equal(len(seeds), 128)
 	})
 }
 
